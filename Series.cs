c78cd0ea4bff51c28c84abc9ea72f97d1f25a4dd
Andrey Gruber <andrey.gruber@gmail.com>
2021-03-31 13:22:49 +0300

    Decreases ChartValue values to 5

--- Series.cs
+++ Series.cs
@@ -162,7 +162,7 @@ namespace ag.WPF.Chart
         {
             foreach (var v in values)
             {
-                _values.Add(new ChartValue((v,0,0,0,0,0,0,0), null));
+                _values.Add(new ChartValue((v,0,0,0,0), null));
             }
 
             initFields(name);
@@ -215,7 +215,7 @@ namespace ag.WPF.Chart
         {
             foreach (var v in values)
             {
-                _values.Add(new ChartValue((v.Value.V1, v.Value.V2, v.Value.V3, v.Value.V4, v.Value.V5, v.Value.V6, v.Value.V7, v.Value.V8), v.CustomValue));
+                _values.Add(new ChartValue((v.Value.V1, v.Value.V2, v.Value.V3, v.Value.V4, v.Value.V5), v.CustomValue));
             }
 
             initFields(name);
@@ -297,73 +297,4 @@ namespace ag.WPF.Chart
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
-
-    ///// <summary>
-    ///// Represents collection of <see cref="ChartValue"/>
-    ///// </summary>
-    //public class ChartValues : ObservableCollection<ChartValue>
-    //{
-    //    internal Path Path { get; set; }
-    //}
-
-    ///// <summary>
-    ///// Represents single chart value
-    ///// </summary>
-    //public class ChartValue
-    //{
-    //    /// <summary>
-    //    /// Gets or sets current numeric value
-    //    /// </summary>
-    //    public (double V1, double V2,double V3,double V4,double V5, double V6, double V7, double V8) Value { get; set; }
-    //    /// <summary>
-    //    /// Gets or sets custom value (usually string) associated with current value. This custom value will be displayed as chart point tooltip
-    //    /// </summary>
-    //    public object CustomValue { get; set; }
-    //    /// <summary>
-    //    /// Initializes a new instance of ChartValue object
-    //    /// </summary>
-    //    /// <param name="V1">Current value</param>
-    //    public ChartValue(double V1)
-    //    {
-    //        Value = (V1, 0, 0, 0, 0, 0, 0, 0);
-    //    }
-    //    public ChartValue(double V1, double V2)
-    //    {
-    //        Value = (V1, V2, 0, 0, 0, 0, 0, 0);
-    //    }
-    //    public ChartValue(double V1, double V2,double V3)
-    //    {
-    //        Value = (V1, V2, V3, 0, 0, 0, 0, 0);
-    //    }
-    //    public ChartValue(double V1, double V2,double V3,double V4)
-    //    {
-    //        Value = (V1, V2, V3, V4, 0, 0, 0, 0);
-    //    }
-    //    public ChartValue(double V1, double V2, double V3, double V4, double V5)
-    //    {
-    //        Value = (V1, V2, V3, V4, V5, 0, 0, 0);
-    //    }
-    //    public ChartValue(double V1, double V2, double V3, double V4, double V5,double V6)
-    //    {
-    //        Value = (V1, V2, V3, V4, V5, V6, 0, 0);
-    //    }
-    //    public ChartValue(double V1, double V2, double V3, double V4, double V5, double V6,double V7)
-    //    {
-    //        Value = (V1, V2, V3, V4, V5, V6, V7, 0);
-    //    }
-    //    public ChartValue(double V1, double V2, double V3, double V4, double V5, double V6, double V7,double V8)
-    //    {
-    //        Value = (V1, V2, V3, V4, V5, V6, V7, V8);
-    //    }
-    //    /// <summary>
-    //    /// Initializes a new instance of ChartValue object
-    //    /// </summary>
-    //    /// <param name="value">Current value</param>
-    //    /// <param name="customValue">Current custom value</param>
-    //    public ChartValue((double V1, double V2, double V3, double V4, double V5, double V6, double V7, double V8) values, object customValue)
-    //        : this(values.V1, values.V2, values.V3, values.V4, values.V5, values.V6, values.V7, values.V8)
-    //    {
-    //        CustomValue = customValue;
-    //    }
-    //}
 }
--- Values/ChartValues.cs
+++ Values/ChartValues.cs
@@ -24,7 +24,7 @@ namespace ag.WPF.Chart.Values
         /// <summary>
         /// Gets or sets current numeric value
         /// </summary>
-        public (double V1, double V2, double V3, double V4, double V5, double V6, double V7, double V8) Value { get; set; }
+        public (double V1, double V2, double V3, double V4, double V5) Value { get; set; }
         /// <summary>
         /// Gets or sets custom value (usually string) associated with current value. This custom value will be displayed as chart point tooltip
         /// </summary>
@@ -35,43 +35,71 @@ namespace ag.WPF.Chart.Values
         /// <param name="V1">Current value</param>
         public ChartValue(double V1)
         {
-            Value = (V1, 0, 0, 0, 0, 0, 0, 0);
+            Value = (V1, 0, 0, 0, 0);
         }
         public ChartValue(double V1, double V2)
         {
-            Value = (V1, V2, 0, 0, 0, 0, 0, 0);
+            Value = (V1, V2, 0, 0, 0);
         }
         public ChartValue(double V1, double V2, double V3)
         {
-            Value = (V1, V2, V3, 0, 0, 0, 0, 0);
+            Value = (V1, V2, V3, 0, 0);
         }
         public ChartValue(double V1, double V2, double V3, double V4)
         {
-            Value = (V1, V2, V3, V4, 0, 0, 0, 0);
+            Value = (V1, V2, V3, V4, 0);
         }
         public ChartValue(double V1, double V2, double V3, double V4, double V5)
         {
-            Value = (V1, V2, V3, V4, V5, 0, 0, 0);
-        }
-        public ChartValue(double V1, double V2, double V3, double V4, double V5, double V6)
-        {
-            Value = (V1, V2, V3, V4, V5, V6, 0, 0);
-        }
-        public ChartValue(double V1, double V2, double V3, double V4, double V5, double V6, double V7)
-        {
-            Value = (V1, V2, V3, V4, V5, V6, V7, 0);
-        }
-        public ChartValue(double V1, double V2, double V3, double V4, double V5, double V6, double V7, double V8)
-        {
-            Value = (V1, V2, V3, V4, V5, V6, V7, V8);
+            Value = (V1, V2, V3, V4, V5);
         }
         /// <summary>
         /// Initializes a new instance of ChartValue object
         /// </summary>
         /// <param name="value">Current value</param>
         /// <param name="customValue">Current custom value</param>
-        public ChartValue((double V1, double V2, double V3, double V4, double V5, double V6, double V7, double V8) values, object customValue)
-            : this(values.V1, values.V2, values.V3, values.V4, values.V5, values.V6, values.V7, values.V8)
+        public ChartValue(double value, object customValue)
+            : this(value)
+        {
+            CustomValue = customValue;
+        }
+        /// <summary>
+        /// Initializes a new instance of ChartValue object
+        /// </summary>
+        /// <param name="values">Current values</param>
+        /// <param name="customValue">Current custom value</param>
+        public ChartValue((double V1, double V2) values, object customValue)
+            : this(values.V1, values.V2)
+        {
+            CustomValue = customValue;
+        }
+        /// <summary>
+        /// Initializes a new instance of ChartValue object
+        /// </summary>
+        /// <param name="values">Current values</param>
+        /// <param name="customValue">Current custom value</param>
+        public ChartValue((double V1, double V2, double V3) values, object customValue)
+            : this(values.V1, values.V2, values.V3)
+        {
+            CustomValue = customValue;
+        }
+        /// <summary>
+        /// Initializes a new instance of ChartValue object
+        /// </summary>
+        /// <param name="values">Current values</param>
+        /// <param name="customValue">Current custom value</param>
+        public ChartValue((double V1, double V2, double V3, double V4) values, object customValue)
+            : this(values.V1, values.V2, values.V3, values.V4)
+        {
+            CustomValue = customValue;
+        }
+        /// <summary>
+        /// Initializes a new instance of ChartValue object
+        /// </summary>
+        /// <param name="values">Current values</param>
+        /// <param name="customValue">Current custom value</param>
+        public ChartValue((double V1, double V2, double V3, double V4, double V5) values, object customValue)
+            : this(values.V1, values.V2, values.V3, values.V4, values.V5)
         {
             CustomValue = customValue;
         }
